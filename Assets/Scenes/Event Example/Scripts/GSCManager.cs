ce7153b7da05cac53f44f1314d13d01ddac6ccf7
yubincho3 <yubincho3@naver.com>
2023-09-17 16:33:18 +0900

    Reinforce GSC script error detect

--- Assets/Scenes/Event Example/Scripts/GSCManager.cs
+++ Assets/Scenes/Event Example/Scripts/GSCManager.cs
@@ -8,224 +8,245 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-public class GSCManager : MonoBehaviour
+namespace GSC
 {
-	[Serializable]
-	public class ScriptCallback
+	public class GSCManager : MonoBehaviour
 	{
-		public string name;
-		public UnityEvent onCall;
-	}
-
-	[SerializeField] TMP_Text m_textBox;
-	[SerializeField] TextAsset m_scriptFile;
-	[SerializeField] GameObject m_buttonPrefab;
-	[SerializeField] LayoutGroup m_buttonLayout;
-
-	[SerializeField] string m_startNode;
-	[SerializeField] char m_prefix;
-
-	public List<ScriptCallback> callbacks;
-
-	const string NamePattern = "[a-zA-Z_][a-zA-Z_0-9]*";
-
-	readonly Queue<GameObject> m_buttonObjectPool = new();
-	readonly Dictionary<string, int> m_nodeLineDict = new();
-
-	string[] m_scriptLines;
-	int m_lineIndex;
-
-	bool m_buttonPressed = false;
-
-	void Start()
-	{
-		string scriptText = m_scriptFile.text.Replace("\r", "");
-		m_scriptLines = scriptText.Split("\n");
-
-		// Parse script nodes
-		for (int i = 0; i < m_scriptLines.Length; i++)
+		[Serializable]
+		public class ScriptCallback
 		{
-			string line = m_scriptLines[i];
-
-			if (Regex.IsMatch(line, $@"^{m_prefix}node {NamePattern}$"))
-			{
-				var spliited = line.Split(' ');
-
-				if (spliited.Length < 2)
-				{
-					Debug.LogError($"(GSC)Invalid node syntax: line {i + 1}");
-					return;
-				}
-
-				if (m_nodeLineDict.ContainsKey(spliited[1]))
-				{
-					Debug.LogError($"(GSC)Multiple node name definition: line {i + 1}");
-					return;
-				}
-
-				m_nodeLineDict.Add(spliited[1], i);
-			}
+			public string name;
+			public UnityEvent onCall;
 		}
 
-		m_textBox.text = string.Empty;
+		[SerializeField] TMP_Text m_textBox;
+		[SerializeField] TextAsset m_GSCScript;
+		[SerializeField] GameObject m_buttonPrefab;
+		[SerializeField] LayoutGroup m_buttonLayout;
 
-		StartCoroutine(StartScript());
-	}
+		[SerializeField] string m_startNode;
+		[SerializeField] char m_prefix;
 
-	IEnumerator StartScript()
-	{
-		if (!m_nodeLineDict.TryGetValue(m_startNode, out m_lineIndex))
+		public List<ScriptCallback> callbacks;
+
+		const string NamePattern = "[a-zA-Z_][a-zA-Z_0-9]*";
+
+		readonly Queue<GameObject> m_buttonObjectPool = new();
+		readonly Dictionary<string, int> m_nodeLineDict = new();
+
+		string[] m_scriptLines;
+		int m_lineIndex;
+
+		bool m_buttonPressed = false;
+
+		void Start()
 		{
-			Debug.LogError($"(GSC)Invalid start node name: ");
-			yield break;
-		}
+			string scriptText = m_GSCScript.text.Replace("\r", "");
+			m_scriptLines = scriptText.Split("\n");
 
-		string nowNode = m_startNode;
+			string nowNode = string.Empty;
+			bool ifStateEnded = true;
 
-		Debug.Log($"(GSC)Start script: node {nowNode}, line {m_lineIndex + 1}");
-
-		while (m_lineIndex < m_scriptLines.Length)
-		{
-			string rawLine = m_scriptLines[m_lineIndex++];
-			string line = rawLine.Trim();
-
-			if (line == string.Empty)
-				continue;
-
-			// Prefix check: Is not a command line?
-			if (!Regex.IsMatch(line, $"{m_prefix}.*"))
+			// Parse script nodes and check if-waitif
+			for (int i = 0; i < m_scriptLines.Length; i++)
 			{
-				m_textBox.text += rawLine;
-				continue;
-			}
+				string line = m_scriptLines[i].Trim();
 
-			// Remove prefix from command
-			line = line.TrimStart(m_prefix);
-
-			// Check command regexes
-			if (Regex.IsMatch(line, $"^goto {NamePattern}$"))
-			{
-				string nextNode = line.Split()[1];
-
-				if (!m_nodeLineDict.TryGetValue(nextNode, out int nextNodeIndex))
+				if (Regex.IsMatch(line, $"^{m_prefix}if \".*\" -> {NamePattern}$"))
+					ifStateEnded = false;
+				else if (Regex.IsMatch(line, $"^{m_prefix}waitif$"))
 				{
-					Debug.LogError($"(GSC)Invalid node name: {nextNode} line {m_lineIndex}");
-					break;
-				}
-
-				nowNode = GotoNode(nextNodeIndex);
-			}
-			else if (Regex.IsMatch(line, $"^if \".*\" -> {NamePattern}$"))
-			{
-				string buttonText = Regex.Match(line, "(\".*\")").Groups[1].Value.Trim('"');
-				string nextNode = line.Split("-> ")[1];
-
-				if (!m_nodeLineDict.TryGetValue(nextNode, out int nextNodeIndex))
-				{
-					Debug.LogError($"(GSC)Invalid node name: {nextNode} line {m_lineIndex}");
-					break;
-				}
-
-				// Instantiate and set button as children of button layout, or dequeue
-				if (!m_buttonObjectPool.TryDequeue(out GameObject buttonObject))
-					buttonObject = Instantiate(m_buttonPrefab, m_buttonLayout.transform);
-
-				buttonObject.transform.SetParent(m_buttonLayout.transform);
-				buttonObject.SetActive(true);
-
-				// Set button onClick events
-				Button buttonComponent = buttonObject.GetComponent<Button>();
-
-				buttonComponent.onClick.RemoveAllListeners();
-				buttonComponent.onClick.AddListener(() =>
-				{
-					nowNode = GotoNode(nextNodeIndex);
-					m_buttonPressed = true;
-				});
-
-				buttonObject.GetComponentInChildren<TMP_Text>().text = buttonText;
-			}
-			else if (Regex.IsMatch(line, "^waitif"))
-			{
-				if (m_buttonLayout.GetComponentsInChildren<Button>().Length == 0)
-				{
-					Debug.LogError($"(GSC)No active if found: line {m_lineIndex}");
-					break;
-				}
-
-				yield return new WaitUntil(() => m_buttonPressed);
-				m_buttonPressed = false;
-			}
-			else if (Regex.IsMatch(line, $"^call {NamePattern}$"))
-			{
-				string callbackName = line.Split()[1];
-				bool callbackNotFound = true;
-
-				foreach (var callback in callbacks)
-				{
-					if (callback.name == callbackName)
+					// If statement has already ended, but multiple waitif detected.
+					if (ifStateEnded)
 					{
-						callback.onCall.Invoke();
-						callbackNotFound = false;
+						Debug.LogError($"(GSC)Multiple 'waitif' detected: line {i + 1}");
+						return;
+					}
+
+					ifStateEnded = true;
+				}
+				else if (Regex.IsMatch(line, $@"^{m_prefix}node {NamePattern}$"))
+				{
+					// Add node to dictionary
+					string nodeName = line.Split()[1];
+
+					if (m_nodeLineDict.ContainsKey(nodeName))
+					{
+						Debug.LogError($"(GSC)Multiple node name definition: line {i + 1}");
+						return;
+					}
+
+					if (!ifStateEnded)
+					{
+						Debug.LogError($"(GSC)'if' must end with 'waitif': node {nowNode}");
+						return;
+					}
+
+					m_nodeLineDict.Add(nodeName, i);
+					nowNode = nodeName;
+				}
+			}
+
+			m_textBox.text = string.Empty;
+
+			StartCoroutine(StartScript());
+		}
+
+		IEnumerator StartScript()
+		{
+			if (!m_nodeLineDict.TryGetValue(m_startNode, out m_lineIndex))
+			{
+				Debug.LogError($"(GSC)Invalid start node name: ");
+				yield break;
+			}
+
+			string nowNode = m_startNode;
+
+			Debug.Log($"(GSC)Start script: node {nowNode}, line {m_lineIndex + 1}");
+
+			while (m_lineIndex < m_scriptLines.Length)
+			{
+				string rawLine = m_scriptLines[m_lineIndex++];
+				string line = rawLine.Trim();
+
+				if (line == string.Empty)
+					continue;
+
+				// Prefix check: Is not a command line?
+				if (!Regex.IsMatch(line, $"{m_prefix}.*"))
+				{
+					m_textBox.text += rawLine;
+					continue;
+				}
+
+				// Remove prefix from command
+				line = line.TrimStart(m_prefix);
+
+				// Check command regexes
+				if (Regex.IsMatch(line, $"^goto {NamePattern}$"))
+				{
+					string nextNode = line.Split()[1];
+
+					if (!m_nodeLineDict.TryGetValue(nextNode, out int nextNodeIndex))
+					{
+						Debug.LogError($"(GSC)Invalid node name: {nextNode} line {m_lineIndex}");
+						break;
+					}
+
+					nowNode = GotoNode(nextNodeIndex);
+				}
+				else if (Regex.IsMatch(line, $"^if \".*\" -> {NamePattern}$"))
+				{
+					string buttonText = Regex.Match(line, "(\".*\")").Groups[1].Value.Trim('"');
+					string nextNode = line.Split("-> ")[1];
+
+					if (!m_nodeLineDict.TryGetValue(nextNode, out int nextNodeIndex))
+					{
+						Debug.LogError($"(GSC)Invalid node name: {nextNode} line {m_lineIndex}");
+						break;
+					}
+
+					// Instantiate and set button as children of button layout, or dequeue
+					if (!m_buttonObjectPool.TryDequeue(out GameObject buttonObject))
+						buttonObject = Instantiate(m_buttonPrefab, m_buttonLayout.transform);
+
+					buttonObject.transform.SetParent(m_buttonLayout.transform);
+					buttonObject.SetActive(true);
+
+					// Set button onClick events
+					Button buttonComponent = buttonObject.GetComponent<Button>();
+
+					buttonComponent.onClick.RemoveAllListeners();
+					buttonComponent.onClick.AddListener(() =>
+					{
+						nowNode = GotoNode(nextNodeIndex);
+						m_buttonPressed = true;
+					});
+
+					buttonObject.GetComponentInChildren<TMP_Text>().text = buttonText;
+				}
+				else if (Regex.IsMatch(line, "^waitif"))
+				{
+					if (m_buttonLayout.GetComponentsInChildren<Button>().Length == 0)
+					{
+						Debug.LogError($"(GSC)No active if found: line {m_lineIndex}");
+						break;
+					}
+
+					yield return new WaitUntil(() => m_buttonPressed);
+					m_buttonPressed = false;
+				}
+				else if (Regex.IsMatch(line, $"^call {NamePattern}$"))
+				{
+					string callbackName = line.Split()[1];
+					bool callbackNotFound = true;
+
+					foreach (var callback in callbacks)
+					{
+						if (callback.name == callbackName)
+						{
+							callback.onCall.Invoke();
+							callbackNotFound = false;
+							break;
+						}
+					}
+
+					if (callbackNotFound)
+					{
+						Debug.LogError($"(GSC)Invalid callback name: {callbackName}");
 						break;
 					}
 				}
-
-				if (callbackNotFound)
+				else if (Regex.IsMatch(line, $"^scene \".*\"$"))
 				{
-					Debug.LogError($"(GSC)Invalid callback name: {callbackName}");
+					string scenePath = line[(line.IndexOf(' ') + 1)..].Trim('"');
+
+					if (SceneUtility.GetBuildIndexByScenePath(scenePath + "") != -1)
+						SceneManager.LoadScene(scenePath);
+					else
+						Debug.LogError($"(GSC)Invalid scene path: {scenePath}");
+
+					break;
+				}
+				else if (Regex.IsMatch(line, $"^node {NamePattern}$"))
+				{
+					string nodeName = line.Split()[1];
+
+					if (nodeName != nowNode)
+						break;
+				}
+				else
+				{
+					Debug.LogError($"Invalid command line: \"{rawLine}\"");
 					break;
 				}
 			}
-			else if (Regex.IsMatch(line, $"^scene \".*\"$"))
-			{
-				string scenePath = line[(line.IndexOf(' ') + 1)..].Trim('"');
 
-				if (SceneUtility.GetBuildIndexByScenePath(scenePath + "") != -1)
-					SceneManager.LoadScene(scenePath);
-				else
-					Debug.LogError($"(GSC)Invalid scene path: {scenePath}");
-
-				break;
-			}
-			else if (Regex.IsMatch(line, $"^node {NamePattern}$"))
-			{
-				string nodeName = line.Split()[1];
-
-				if (nodeName != nowNode)
-					break;
-			}
-			else
-			{
-				Debug.LogError($"Invalid command line: \"{rawLine}\"");
-				break;
-			}
+			Debug.Log($"(GSC)End script: node {nowNode}, line {m_lineIndex}");
 		}
 
-		Debug.Log($"(GSC)End script: node {nowNode}, line {m_lineIndex}");
-	}
-
-	// Goto the node in line nodeLineIndex and return that node name.
-	string GotoNode(int nodeLineIndex)
-	{
-		// Clear textbox
-		m_textBox.text = string.Empty;
-
-		// Clear button layout
-		foreach (var childButton in m_buttonLayout.GetComponentsInChildren<Button>())
+		// Goto the node in line nodeLineIndex and return that node name.
+		string GotoNode(int nodeLineIndex)
 		{
-			var childObject = childButton.gameObject;
+			// Clear textbox
+			m_textBox.text = string.Empty;
 
-			childObject.SetActive(false);
-			childObject.transform.SetParent(null);
-			m_buttonObjectPool.Enqueue(childObject);
+			// Clear button layout
+			foreach (var childButton in m_buttonLayout.GetComponentsInChildren<Button>())
+			{
+				var childObject = childButton.gameObject;
+
+				childObject.SetActive(false);
+				childObject.transform.SetParent(null);
+				m_buttonObjectPool.Enqueue(childObject);
+			}
+
+			// Goto node
+			m_lineIndex = nodeLineIndex;
+
+			// Return node name
+			string nodeName = m_scriptLines[m_lineIndex].Split()[1];
+			return nodeName;
 		}
-
-		// Goto node
-		m_lineIndex = nodeLineIndex;
-
-		// Return node name
-		string nodeName = m_scriptLines[m_lineIndex].Split()[1];
-		return nodeName;
 	}
 }
--- Assets/Scenes/Event Example/Scripts/TextTypeEffector.cs
+++ Assets/Scenes/Event Example/Scripts/TextTypeEffector.cs
@@ -2,52 +2,55 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 
-public class TextTypeEffector : MonoBehaviour
+namespace GSC
 {
-	public float m_typeInterval;
-	public TMP_Text m_sourceTextbox;
-	public TMP_Text m_destinationTextbox;
-
-	IEnumerator m_typingCoroutine = null;
-
-	string m_prevText = string.Empty;
-	bool m_forceCompleteType = false;
-
-	void Update()
+	public class TextTypeEffector : MonoBehaviour
 	{
-		// If user clicks mouse0 or screen, force typing to complete.
-		m_forceCompleteType = (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) ||
-							   Input.GetKeyUp(KeyCode.Mouse0);
+		public float m_typeInterval;
+		public TMP_Text m_sourceTextbox;
+		public TMP_Text m_destinationTextbox;
 
-		if (m_prevText != m_sourceTextbox.text)
+		IEnumerator m_typingCoroutine = null;
+
+		string m_prevText = string.Empty;
+		bool m_forceCompleteType = false;
+
+		void Update()
 		{
-			m_prevText = m_sourceTextbox.text;
+			// If user clicks mouse0 or screen, force typing to complete.
+			m_forceCompleteType = (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) ||
+								   Input.GetKeyUp(KeyCode.Mouse0);
 
-			if (m_typingCoroutine is not null)
-				StopCoroutine(m_typingCoroutine);
-
-			m_typingCoroutine = StartTyping();
-			StartCoroutine(m_typingCoroutine);
-		}
-	}
-
-	IEnumerator StartTyping()
-	{
-		string text = m_sourceTextbox.text;
-
-		m_destinationTextbox.text = string.Empty;
-		m_forceCompleteType = false;
-
-		foreach (char ch in text)
-		{
-			if (m_forceCompleteType)
+			if (m_prevText != m_sourceTextbox.text)
 			{
-				m_destinationTextbox.text = text;
-				yield break;
-			}
+				m_prevText = m_sourceTextbox.text;
 
-			yield return new WaitForSeconds(m_typeInterval);
-			m_destinationTextbox.text += ch;
+				if (m_typingCoroutine is not null)
+					StopCoroutine(m_typingCoroutine);
+
+				m_typingCoroutine = StartTyping();
+				StartCoroutine(m_typingCoroutine);
+			}
+		}
+
+		IEnumerator StartTyping()
+		{
+			string text = m_sourceTextbox.text;
+
+			m_destinationTextbox.text = string.Empty;
+			m_forceCompleteType = false;
+
+			foreach (char ch in text)
+			{
+				if (m_forceCompleteType)
+				{
+					m_destinationTextbox.text = text;
+					yield break;
+				}
+
+				yield return new WaitForSeconds(m_typeInterval);
+				m_destinationTextbox.text += ch;
+			}
 		}
 	}
 }
