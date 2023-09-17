f3c4b80d60f7e9e9ce6f531470a9c03a7b14785c
yubincho3 <yubincho3@naver.com>
2023-09-17 12:32:47 +0900

    Intial commit of MapScene

--- Assets/Scenes/MapScenes/Scripts/CanditiateManager.cs
+++ Assets/Scenes/MapScenes/Scripts/CanditiateManager.cs
@@ -3,39 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class CanditiateManager : MonoBehaviour
 {
 	static readonly System.Random m_systemRandom = new();
 
-	public List<GameObject> m_canditiateGroups;
 	public GameObject m_midBossPrefab;
 	public GameObject m_battlePrefab;
 	public GameObject m_randomPrefab;
 	public GameObject m_chestPrefab;
 
-	readonly List<GameObject> m_selectedArea = new();
+	public GameObject m_startArea;
+	public GameObject temp;
+	public float m_areaDistMax = 130.36f;
 
 	// Start is called before the first frame update
 	void Start()
 	{
+		print((m_startArea.transform.position - temp.transform.position).magnitude);
+
 		int seed = m_systemRandom.Next();
+
 		Random.InitState(seed);
 		Debug.Log($"Seed: {seed}");
 
-		// Choice areas from canditiate groups
-		foreach (GameObject canditiateGroup in m_canditiateGroups)
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_startArea.transform.position, m_areaDistMax);
+
+		print(colliders.Length);
+
+		m_startArea.SetActive(true);
+
+		foreach (var temp in colliders)
 		{
-			Debug.Assert(canditiateGroup.transform.childCount > 0, "Can't find canditiates from groups.");
-
-			int areaSelected = Random.Range(0, canditiateGroup.transform.childCount);
-
-			Transform areaTransform = canditiateGroup.transform.GetChild(areaSelected);
-			m_selectedArea.Add(areaTransform.gameObject);
+			temp.gameObject.SetActive(true);
 		}
 
 		/// ## BELOW IS TEMP! ## ///
+		/*
 		GameObject[] prefabs = {
 			m_midBossPrefab,
 			m_battlePrefab,
@@ -51,8 +57,10 @@ public class CanditiateManager : MonoBehaviour
 
 			PrefabUtility.ConvertToPrefabInstance(area, selectedPrefab, setting, InteractionMode.AutomatedAction);
 		}
+		*/
 	}
 
+	// THIS IS TEMP!
 	public static void OnAreaClick(string areaName)
 	{
 		print(areaName + ": clicked");
