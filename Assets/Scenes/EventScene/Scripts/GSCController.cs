48c87b663be8e77ce9465aae7241fac0ecdd88f7
yubincho3 <yubincho3@naver.com>
2023-10-04 21:48:13 +0900

    Fix GSC cursor infinite loop bug

--- Assets/Scenes/EventScene/Scripts/GSCController.cs
+++ Assets/Scenes/EventScene/Scripts/GSCController.cs
@@ -8,126 +8,124 @@ using UnityEngine.UI;
 
 namespace GSC
 {
-	/// <summary>
-	/// Button is target, TMP_Text(Can be null) is text of button
-	/// </summary>
-	/// <returns>
-	/// Return components tuple of it
-	/// </returns>
-	public delegate Tuple<Button, TMP_Text> ButtonCreator();
+    /// <summary>
+    /// Button is target, TMP_Text(Can be null) is text of button
+    /// </summary>
+    /// <returns>
+    /// Return components tuple of it
+    /// </returns>
+    public delegate Tuple<Button, TMP_Text> ButtonCreator();
 
-	/// <summary>
-	/// Destroy all buttons that created by ButtonCreator
-	/// </summary>
-	public delegate void ButtonCleaner();
+    /// <summary>
+    /// Destroy all buttons that created by ButtonCreator
+    /// </summary>
+    public delegate void ButtonCleaner();
 
-	public class GSCController
-	{
-		readonly List<GSCScriptLine> m_scripts;
-		readonly List<GSCCallback> m_callbacks;
-		readonly GSCTextbox m_textbox;
+    public class GSCController
+    {
+        readonly List<GSCScriptLine> m_scripts;
+        readonly List<GSCCallback> m_callbacks;
+        readonly GSCTextbox m_textbox;
 
-		readonly ButtonCreator m_buttonCreator;
-		readonly ButtonCleaner m_buttonCleaner;
+        readonly ButtonCreator m_buttonCreator;
+        readonly ButtonCleaner m_buttonCleaner;
 
-		// The index of script line to be executed next
-		int m_scriptLineIndex;
+        // The index of script line to be executed next
+        int m_scriptLineIndex;
 
-		// When a button is pressed, the value is set
-		string m_branchTarget = null;
+        // When a button is pressed, the value is set
+        string m_branchTarget = null;
 
-		public GSCController(
-			List<GSCScriptLine> scripts,
-			List<GSCCallback> callbacks,
-			GSCTextbox textbox,
-			ButtonCreator buttonCreator,
-			ButtonCleaner buttonCleaner,
-			string startNode
-		)
-		{
-			m_scripts = scripts;
-			m_callbacks = callbacks;
-			m_textbox = textbox;
+        public GSCController(
+            List<GSCScriptLine> scripts,
+            List<GSCCallback> callbacks,
+            GSCTextbox textbox,
+            ButtonCreator buttonCreator,
+            ButtonCleaner buttonCleaner,
+            string startNode
+        )
+        {
+            m_scripts = scripts;
+            m_callbacks = callbacks;
+            m_textbox = textbox;
 
-			m_buttonCreator = buttonCreator;
-			m_buttonCleaner = buttonCleaner;
+            m_buttonCreator = buttonCreator;
+            m_buttonCleaner = buttonCleaner;
 
-			BranchTo(startNode);
-		}
+            BranchTo(startNode);
+        }
 
-		public GSCScriptLine Next()
-		{
-			// End of script
-			if (m_scriptLineIndex >= m_scripts.Count)
-			{
-				m_textbox.StartTyping();
-				return null;
-			}
+        public GSCScriptLine Next()
+        {
+            // End of script
+            if (m_scriptLineIndex >= m_scripts.Count)
+            {
+                m_textbox.StartTyping();
+                return null;
+            }
 
-			return m_scripts[m_scriptLineIndex];
-		}
+            return m_scripts[m_scriptLineIndex++];
+        }
 
-		public void AddText(string text) =>
-			m_textbox.AddText(text);
+        public void AddText(string text) =>
+            m_textbox.AddText(text);
 
-		public void BranchTo(string nodeName)
-		{
-			m_scriptLineIndex = 0;
+        public void BranchTo(string nodeName)
+        {
+            for (m_scriptLineIndex = 0; m_scriptLineIndex < m_scripts.Count; m_scriptLineIndex++)
+            {
+                var now = m_scripts[m_scriptLineIndex];
+                if (now.Command == GSCCommand.Node && now.Args[0] == nodeName)
+                    return;
+            }
 
-			while (m_scriptLineIndex < m_scripts.Count)
-			{
-				var now = m_scripts[m_scriptLineIndex];
-				if (now.Command == GSCCommand.Node && now.Args[0] == nodeName)
-					return;
-			}
+            throw new UnityException($"(GSC)Invalid node name: {nodeName}");
+        }
 
-			throw new UnityException($"(GSC)Invalid node name: {nodeName}");
-		}
+        public void AddConditional(string description, string branchTarget)
+        {
+            var (button, textbox) = m_buttonCreator.Invoke();
 
-		public void AddConditional(string description, string branchTarget)
-		{
-			var (button, textbox) = m_buttonCreator.Invoke();
+            button.onClick.AddListener(() => m_branchTarget ??= branchTarget);
 
-			button.onClick.AddListener(() => m_branchTarget ??= branchTarget);
+            if (textbox != null)
+                textbox.SetText(description);
+        }
 
-			if (textbox != null)
-				textbox.SetText(description);
-		}
+        public IEnumerator BranchConditional()
+        {
+            m_textbox.StartTyping();
 
-		public IEnumerator BranchConditional()
-		{
-			m_textbox.StartTyping();
+            yield return new WaitWhile(() => m_branchTarget is null);
 
-			yield return new WaitWhile(() => m_branchTarget is null);
+            m_buttonCleaner.Invoke();
+            m_textbox.Clear();
 
-			m_buttonCleaner.Invoke();
-			m_textbox.Clear();
+            BranchTo(m_branchTarget);
+            m_branchTarget = null;
+        }
 
-			BranchTo(m_branchTarget);
-			m_branchTarget = null;
-		}
+        public void InvokeCallback(string name)
+        {
+            foreach (var callback in m_callbacks)
+            {
+                if (callback.name == name)
+                {
+                    callback.onCall.Invoke();
+                    return;
+                }
+            }
 
-		public void InvokeCallback(string name)
-		{
-			foreach (var callback in m_callbacks)
-			{
-				if (callback.name == name)
-				{
-					callback.onCall.Invoke();
-					return;
-				}
-			}
+            Debug.LogWarning($"(GSC)Callback not found: {name}");
+        }
 
-			Debug.LogWarning($"(GSC)Callback not found: {name}");
-		}
+        // Is it OK...?
+        public void LoadScene(string sceneName)
+        {
+            if (SceneUtility.GetBuildIndexByScenePath(sceneName) == -1)
+                throw new UnityException($"(GSC)Scene not exist: {sceneName}");
 
-		// Is it OK...?
-		public void LoadScene(string sceneName)
-		{
-			if (SceneUtility.GetBuildIndexByScenePath(sceneName) == -1)
-				throw new UnityException($"(GSC)Scene not exist: {sceneName}");
-
-			SceneManager.LoadSceneAsync(sceneName);
-		}
-	}
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
 }
