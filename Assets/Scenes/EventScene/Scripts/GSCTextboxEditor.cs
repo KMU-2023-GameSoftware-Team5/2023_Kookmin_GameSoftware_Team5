06c74fbc70733d1335b1b7eb5516d082a72e51e6
yubincho3 <yubincho3@naver.com>
2023-10-06 12:28:10 +0900

    Add Hangeul supported free font

--- Assets/Scenes/EventScene/Scripts/GSCTextboxEditor.cs
+++ Assets/Scenes/EventScene/Scripts/GSCTextboxEditor.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 namespace GSC
 {
-	[CustomEditor(typeof(GSCTextbox))]
-	[CanEditMultipleObjects]
-	public class GSCTextboxEditor : TMP_EditorPanelUI
-	{
-		SerializedProperty m_typeInterval;
+    [CustomEditor(typeof(GSCTextbox))]
+    [CanEditMultipleObjects]
+    public class GSCTextboxEditor : TMP_EditorPanelUI
+    {
+        SerializedProperty m_typeInterval;
+        SerializedProperty m_completeOnClick;
 
-		protected override void OnEnable()
-		{
-			m_typeInterval = serializedObject.FindProperty("m_typeInterval");
+        protected override void OnEnable()
+        {
+            m_typeInterval = serializedObject.FindProperty("m_typeInterval");
+            m_completeOnClick = serializedObject.FindProperty("m_completeOnClick");
 
-			base.OnEnable();
-		}
+            base.OnEnable();
+        }
 
-		public override void OnInspectorGUI()
-		{
-			serializedObject.Update();
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
 
-			Rect rect = EditorGUILayout.GetControlRect(false, 22);
-			GUI.Label(rect, new GUIContent("<b>GSC Textbox Settings</b>"), TMP_UIStyleManager.sectionHeader);
+            Rect rect = EditorGUILayout.GetControlRect(false, 22);
+            GUI.Label(rect, new GUIContent("<b>GSC Textbox Settings</b>"), TMP_UIStyleManager.sectionHeader);
 
-			EditorGUILayout.PropertyField(m_typeInterval);
-			serializedObject.ApplyModifiedProperties();
+            EditorGUILayout.PropertyField(m_completeOnClick);
+            EditorGUILayout.PropertyField(m_typeInterval);
+            serializedObject.ApplyModifiedProperties();
 
-			base.OnInspectorGUI();
-		}
-	}
+            base.OnInspectorGUI();
+        }
+    }
 }
